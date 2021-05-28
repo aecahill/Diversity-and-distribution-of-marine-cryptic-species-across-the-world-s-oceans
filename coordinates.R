5182c0aa85d4f9bde80d69bbb299eeec89f647db
Marktran21 <84835250+Marktran21@users.noreply.github.com>
2021-05-28 23:08:27 +0700

    Add files via upload

---
+++ coordinates.R
@@ -0,0 +1,34 @@
+rm(list=ls())
+
+
+library(dplyr)
+library(robis)
+library(readr)
+library(ggplot2)
+library(vegan)
+library(mregions)
+
+
+
+eez <- mr_names("MarineRegions:eez")
+iho <- mr_names('MarineRegions:iho')
+fao <- mr_names('MarineRegions:fao')
+lme <- mr_names('MarineRegions:lme')
+
+
+#Get the polygon for the North sea
+NS<-mr_shp(key = "MarineRegions:lme", filter= "North Sea", read= TRUE)
+Northsea<-mr_as_wkt(NS)
+
+#Get the occurrence in that polygon but failed
+occurrence(geometry= Northsea )
+
+#try mapping the polygon
+world <- map_data("world")
+ggplot() +
+  geom_polygon(data = world, aes(x = long, y = lat, group = group), fill = "#dddddd") +
+  geom_polygon(data = NS, aes(x = long, y = lat, group = group), fill = "#cc3300") +
+  coord_fixed(1)
+
+
+NAtlantic<-mr_shp(key = "MarineRegions:eez_iho_union_v2", filter= "North Atlantic Ocean", read= TRUE)
